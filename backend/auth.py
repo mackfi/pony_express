6f45c5cfe74458f55f6e2ac86378d2bd0188dd68
Mack Fischer <mackfisch@gmail.com>
2024-03-17 19:03:33 -0600

    More or less mplemented both auth routes entirely. May revisit to test more thoroughly.

--- backend/auth.py
+++ backend/auth.py
@@ -1,18 +1,27 @@
-from passlib.context import CryptContext
-from sqlmodel import SQLModel, Session
-from fastapi.security import OAuth2PasswordBearer 
-from fastapi import APIRouter, Depends
+import os
+from datetime import datetime, timezone
 from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import (
+    OAuth2PasswordBearer,
+    OAuth2PasswordRequestForm,
+)
+from jose import ExpiredSignatureError, JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel, ValidationError
+from sqlmodel import Session, SQLModel, select
 
 from backend import database as db
 
 from backend.schema import UserInDB, User, UserResponse
 from backend.database import DuplicateEntityException
 
-import os
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 jwt_key = os.environ.get("JWT_KEY", default="any string you want for a dev JWT key")
+jwt_alg = "HS256"
+access_token_duration = 3600  # seconds
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
@@ -25,6 +34,61 @@ class UserRegistration(SQLModel):
     email: str
     password: str
 
+class AccessToken(BaseModel):
+    """Response model for access token."""
+
+    access_token: str
+    token_type: str
+    expires_in: int
+
+
+class Claims(BaseModel):
+    """Access token claims (aka payload)."""
+
+    sub: str  # id of user
+    exp: int  # unix timestamp
+
+
+class AuthException(HTTPException):
+    def __init__(self, error: str, description: str):
+        super().__init__(
+            status_code=401,
+            detail={
+                "error": error,
+                "error_description": description,
+            },
+        )
+
+
+class InvalidCredentials(AuthException):
+    def __init__(self):
+        super().__init__(
+            error="invalid_client",
+            description="invalid username or password",
+        )
+
+
+class InvalidToken(AuthException):
+    def __init__(self):
+        super().__init__(
+            error="invalid_client",
+            description="invalid bearer token",
+        )
+
+
+class ExpiredToken(AuthException):
+    def __init__(self):
+        super().__init__(
+            error="invalid_client",
+            description="expired access token",
+        )
+
+def get_current_user(
+    session: Session = Depends(db.get_session),
+    token: str = Depends(oauth2_scheme),
+) -> UserInDB:
+    user = _decode_access_token(session, token)
+    return user
 
 @auth_router.post("/registration")
 def register_new_user(registration: UserRegistration, session: Annotated[Session, Depends(db.get_session)]) -> UserResponse:
@@ -46,6 +110,53 @@ def register_new_user(registration: UserRegistration, session: Annotated[Session
         if other_usernames:
             raise DuplicateEntityException(entity_name="User", entity_field="username", entity_value=user.username)
     
-
-
     return UserResponse(user=user)
+
+@auth_router.post("/token")
+def get_access_token(form: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(db.get_session),):
+
+    user = _get_authenticated_user(session, form)
+    return _build_access_token(user)
+
+def _get_authenticated_user(
+    session: Session,
+    form: OAuth2PasswordRequestForm,
+) -> UserInDB:
+    user = session.exec(
+        select(UserInDB).where(UserInDB.username == form.username)
+    ).first()
+
+    if user is None or not pwd_context.verify(form.password, user.hashed_password):
+        raise InvalidCredentials()
+
+    return user
+
+def _build_access_token(user: UserInDB) -> AccessToken:
+    expiration = int(datetime.now(timezone.utc).timestamp()) + access_token_duration
+    claims = Claims(sub=str(user.id), exp=expiration)
+    access_token = jwt.encode(claims.model_dump(), key=jwt_key, algorithm=jwt_alg)
+
+    return AccessToken(
+        access_token=access_token,
+        token_type="Bearer",
+        expires_in=access_token_duration,
+    )
+
+def _decode_access_token(session: Session, token: str) -> UserInDB:
+    try:
+        claims_dict = jwt.decode(token, key=jwt_key, algorithms=[jwt_alg])
+        claims = Claims(**claims_dict)
+        user_id = claims.sub
+        user = session.get(UserInDB, user_id)
+
+        if user is None:
+            raise InvalidToken()
+
+        return user
+    except ExpiredSignatureError:
+        raise ExpiredToken()
+    except JWTError:
+        raise InvalidToken()
+    except ValidationError():
+        raise InvalidToken()
